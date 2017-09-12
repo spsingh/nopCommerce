707b585189c805a8e83282e379fef9421dc4f855
skoshelev <sergey.k@nopcommerce.com>
2017-09-12 14:20:29 +0300

    #2057 Added import/export specification attributes to import/export products

---
+++ src/Libraries/Nop.Services/ExportImport/TypeOfExportedAttribute.cs
@@ -0,0 +1,21 @@
+﻿namespace Nop.Services.ExportImport
+{
+    /// <summary>
+    /// Represents the type of the exported attribute
+    /// </summary>
+    public enum TypeOfExportedAttribute
+    {
+        /// <summary>
+        /// Not specified
+        /// </summary>
+        NotSpecified = 1,
+        /// <summary>
+        /// Product attribute
+        /// </summary>
+        ProductAttribute = 10,
+        /// <summary>
+        /// Specification attribute
+        /// </summary>
+        SpecificationAttribute = 20
+    }
+}
